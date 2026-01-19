d4e201a314e80a995a0bd72e61414f4a3ee015b6
Alex Ribeiro <alex.rbribeiro@gmail.com>
2026-01-19 13:12:24 -0300

    coloquei algus menus e fiz o loop pro menu principal

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <ctype.h>
 #include <string.h>
 #define max 50
-//TESTE
 
 //funcoes
 void limpar_terminal() {
@@ -16,25 +15,68 @@ void limpar_terminal() {
 
 int menu_principal() {
 
+  //Create Read Update Delete
   printf("PLATAFORMA DE GERENCIAMENTO DE ASSINATURAS\n\n");
   printf("Escolha umas das seguintes opcoes:\n");
-  printf("1 - Cadastrar um novo usuario.\n");
-  printf("2 - Cadastrar um novo servico de streaming.\n");
-  printf("3 - Consultar dados de uma assinatura.\n\n");
+  printf("1 - Realizar um cadastro de clientes, plataformas ou assinaturas.\n");
+  printf("2 - Consultar os dados de clientes, plataformas, ou assinaturas.\n");
+  printf("3 - Alterar as informacoes de clientes, plataformas ou assinaturas.\n");
+  printf("4 - Excluir os dados de clientes, plataformas ou assinaturas.\n");
+  printf("5 - Sair\n\n");
 
   int a;
   scanf("%d", &a);
   return a;
 }
 
-int consulta_assinatura(int a) {
+int realizar_cadastro(){
 
-  printf("CONSULTA DE DADOS DE ASSINATURAS\n\n");
-  printf("Escolha umas das seguintes opcoes:\n");
-  printf("1 - Consulta por cliente.\n");
-  printf("2 - Consulta por plataforma.\n");
-  printf("3 - Consultar assinatura especifica.\n\n");
+  printf("O que voce deseja cadastrar?\n\n");
+  printf("1 - Cadastro de clientes.\n");
+  printf("2 - Cadastro de plataformas.\n");
+  printf("3 - Cadastro de assinaturas.\n");
+  printf("4 - Voltar\n\n");
 
+  int a;
+  scanf("%d", &a);
+  return a;
+}
+
+int realizar_consulta(){
+
+  printf("O que voce deseja consultar?\n\n");
+  printf("1 - Consultar clientes.\n");
+  printf("2 - Consultar plataformas.\n");
+  printf("3 - Consultar assinaturas.\n");
+  printf("4 - Voltar\n\n");
+
+  int a;
+  scanf("%d", &a);
+  return a;
+}
+
+int realizar_alteracao(){
+
+  printf("O que voce deseja alterar?\n\n");
+  printf("1 - Alterar dados de clientes.\n");
+  printf("2 - Alterar dados de plataformas.\n");
+  printf("3 - Alterar dados de assinaturas.\n");
+  printf("4 - Voltar\n\n");
+
+  int a;
+  scanf("%d", &a);
+  return a;
+}
+
+int realizar_exclusao(){
+
+  printf("O que voce deseja excluir?\n\n");
+  printf("1 - Excluir clientes.\n");
+  printf("2 - Excluir plataformas.\n");
+  printf("3 - Excluir assinaturas.\n");
+  printf("4 - Voltar\n\n");
+
+  int a;
   scanf("%d", &a);
   return a;
 }
@@ -78,33 +120,110 @@ typedef struct {
 
 int main() {
 
-  int teste;
+  while(1){
+    
+    limpar_terminal();
 
-  teste = menu_principal();
-  limpar_terminal();
+    int input;
 
-  if (teste <= 0 || teste > 3) {
-    while (1) {
-      limpar_terminal();
-      printf("Por favor insira uma opcao valida!\n");
-      teste = menu_principal();
-      if (teste >= 1 && teste <= 3) {
-        break;
+    //escolha principal
+    input = menu_principal();
+    limpar_terminal();
+    
+    //forcando o usuario a escolher uma opcao valida
+    if (input <= 0 || input > 5){
+      while (1) {
+        limpar_terminal();
+        printf("Por favor insira uma opcao valida!\n");
+        input = menu_principal();
+        if(input >= 1 && input <= 5) {
+          break;
+        }
+
       }
-
     }
-  }
-  limpar_terminal();
-
-  if (teste == 1) {
 
     limpar_terminal();
-  } else if (teste == 2) {
 
-    limpar_terminal();
-  } else {
-    consulta_assinatura(teste);
-    limpar_terminal();
+    while(1){
+
+      if(input == 1){
+        input = realizar_cadastro();
+        //forcando o usuario a escolher uma opcao valida
+        if(input <= 0 || input > 4){
+          while (1) {
+            limpar_terminal();
+            printf("Por favor insira uma opcao valida!\n");
+            input = realizar_cadastro();
+            if(input >= 1 && input <= 4){
+              break;
+            }
+
+          }
+        }
+        if(input == 4){
+          break;
+        }
+
+      }else if(input == 2){
+        input = realizar_consulta();
+
+        //forcando o usuario a escolher uma opcao valida
+        if(input <= 0 || input > 4){
+          while (1) {
+            limpar_terminal();
+            printf("Por favor insira uma opcao valida!\n");
+            input = realizar_consulta();
+            if(input >= 1 && input <= 4) {
+              break;
+            }
+          }
+        }
+        if(input == 4){
+          break;
+        }
+
+      }else if(input == 3){
+        input = realizar_alteracao();
+        //forcando o usuario a escolher uma opcao valida
+        if(input <= 0 || input > 4){
+          while (1) {
+            limpar_terminal();
+            printf("Por favor insira uma opcao valida!\n");
+            input = realizar_alteracao();
+            if(input >= 1 && input <= 4) {
+              break;
+            }
+
+          }
+        }
+        if(input == 4){
+            break;
+          }
+      
+      }else if(input == 4){
+        input = realizar_exclusao();
+
+        //forcando o usuario a escolher uma opcao valida
+        if(input <= 0 || input > 4){
+          while (1) {
+            limpar_terminal();
+            printf("Por favor insira uma opcao valida!\n");
+            input = realizar_exclusao();
+            if(input >= 1 && input <= 4) {
+              break;
+            }
+
+          }
+        }
+        if(input == 4){
+          break;
+        }
+
+      }else if(input == 5){
+        return 0;
+      }
+    }
   }
 
   return 0;
